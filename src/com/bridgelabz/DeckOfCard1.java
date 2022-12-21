6f1eda3ca8d304ca73e6a030f36d4e22a9e8f351
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-21 08:56:41 +0530

    Added: To initialize deck of cards having suit

--- src/com/bridgelabz/DeckOfCard1.java
+++ src/com/bridgelabz/DeckOfCard1.java
@@ -6,7 +6,7 @@ package com.bridgelabz;
 ("Clubs", "Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5", "6", "7", "8",
 "9", "10", "Jack", "Queen", "King", "Ace"). Shuffle the cards using Random
 method and then distribute 9 Cards to 4 Players and Print the Cards received by
-the 4 Players using 2D Array...
+the 4 Players using 2D Array....
  */
 public class DeckOfCard1 {
     String[] deck = new String[52];// Total cards
